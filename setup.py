85b4fa3d5b748bc61c24b8b1c593c46081c77407
Eric Faurot <eric@faurot.net>
2019-05-09 20:51:36 +0200

    work on 0.12

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name="udon",
       description="Simple helpers for python apps",
-      version="0.11.0",
+      version="0.12-dev",
       author="Eric Faurot",
       author_email="eric@faurot.net",
       url="https://github.com/ericfaurot/udon",
